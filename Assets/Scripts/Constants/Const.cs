b13876d15893bda58a85e3a61b6ab591d2446ad9
exdunn <dkvcio2n!>
2017-02-22 13:45:48 -0800

    remove imported skybox add new skybox import add floor texture add Q, E rotation remove mouse rotation

--- Assets/Scripts/Constants/Const.cs
+++ Assets/Scripts/Constants/Const.cs
@@ -2,7 +2,7 @@
 {
 	public static float MOVESPEED = 0.25f;
     public static float JUMPSPEED = 200.0F;
-    public static float ROTSPEED = 5.0F;
+    public static float ROTSPEED = 1.5F;
     public static float GRAVITY = 20.0F;
     public static float COOLDOWN = 0.7f;
     public static float SPAWNRATE = 5.0f;
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,19 +6,19 @@ public class PlayerController : MonoBehaviour
 {
 
     private GameObject staff;
-    private Vector3 middleOfScreen;
 
     void Awake()
     {
-        middleOfScreen = new Vector3(Screen.width / 2, Screen.height / 2, 0f);
         staff = transform.Find("Staff").gameObject;
     }
 
-    void Update()
+    void FixedUpdate()
     {
+        float rotation = Input.GetAxis("Rotation Axis");
         float moveY = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
         Rigidbody rb = GetComponent<Rigidbody>();
+
 
         // movement based on WASD
         if (moveY != 0)
@@ -29,12 +29,13 @@ public class PlayerController : MonoBehaviour
         {
             rb.MovePosition(rb.transform.position + rb.transform.forward * moveZ * Const.MOVESPEED);
         }
-
-        // mouse rotation
-        Vector3 camVec = Input.mousePosition - middleOfScreen;
-        Vector3 flipped = new Vector3(camVec.x, 0f, camVec.y);
-        transform.LookAt(flipped);
-
+        if (rotation != 0)
+        {
+            Vector3 rotationY = new Vector3(0f, rotation, 0f);
+            rotationY = rotationY * Const.ROTSPEED;
+            Quaternion deltaRotation = Quaternion.Euler(rotationY);
+            rb.MoveRotation(rb.rotation * deltaRotation);
+        }
         /*if (Input.GetAxis("Vertical") != 0)
         {
             staff.GetComponent<Animator>().SetBool("Moving", true);
