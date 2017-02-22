8c2b62d5f6d95932e49b903f281df2d36dde697d
exdunn <dkvcio2n!>
2017-02-22 13:08:12 -0800

    add skybox add strafing add mouse rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,19 +6,36 @@ public class PlayerController : MonoBehaviour
 {
 
     private GameObject staff;
+    private Vector3 middleOfScreen;
 
     void Awake()
     {
+        middleOfScreen = new Vector3(Screen.width / 2, Screen.height / 2, 0f);
         staff = transform.Find("Staff").gameObject;
     }
 
-    void LateUpdate()
+    void Update()
     {
-        float rotateY = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
         Rigidbody rb = GetComponent<Rigidbody>();
 
-        if (Input.GetAxis("Vertical") != 0)
+        // movement based on WASD
+        if (moveY != 0)
+        {
+            rb.MovePosition(rb.transform.position + rb.transform.right * moveY * Const.MOVESPEED);
+        }
+        if (moveZ != 0)
+        {
+            rb.MovePosition(rb.transform.position + rb.transform.forward * moveZ * Const.MOVESPEED);
+        }
+
+        // mouse rotation
+        Vector3 camVec = Input.mousePosition - middleOfScreen;
+        Vector3 flipped = new Vector3(camVec.x, 0f, camVec.y);
+        transform.LookAt(flipped);
+
+        /*if (Input.GetAxis("Vertical") != 0)
         {
             staff.GetComponent<Animator>().SetBool("Moving", true);
             GetComponent<Animator>().SetBool("Moving", true);
@@ -27,20 +44,12 @@ public class PlayerController : MonoBehaviour
         {
             staff.GetComponent<Animator>().SetBool("Moving", false);
             GetComponent<Animator>().SetBool("Moving", false);
-        }
+        }*/
 
         if (GetComponent<PlayerCollision>().getCanJump() && Input.GetButton("Jump"))
         {
             GetComponent<Rigidbody>().AddForce(Vector3.up * Const.JUMPSPEED);
             GetComponent<PlayerCollision>().canJumpFalse();
         }
-
-        Vector3 rotationY = new Vector3(0f, rotateY, 0f);
-        rotationY = rotationY.normalized * Const.ROTSPEED;
-        Quaternion deltaRotation = Quaternion.Euler(rotationY);
-        rb.MoveRotation(rb.rotation * deltaRotation);
-
-        rb.MovePosition (rb.transform.position + rb.transform.forward * moveZ * Const.MOVESPEED);
-        
     }
 }
